891f5dfb10e3413e4492055ea90be092cffcf7ce
jahidulkanchan <mdj524156@gmail.com>
2025-12-29 12:25:03 +0600

    add video

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LightEffect from "../../assets/videos/run-light-effect.mp4";
+import LightEffect from "../../assets/videos/run-light-effect.mp4"
 import HeroBanner from "../../assets/images/hero-banner.png";
 
 export default function HeroSection() {
@@ -7,7 +7,7 @@ export default function HeroSection() {
     <>
       <section className="relative bg-dark">
         <video
-          className="absolute top-0 left-0 z-[-1] h-full w-full object-cover"
+          className="absolute top-0 left-0 z-[0] h-full w-full object-cover"
           muted
           loop
           autoPlay
@@ -15,7 +15,7 @@ export default function HeroSection() {
         ></video>
       
         <div className="z-10 container px-3 py-10 mx-auto gap-5 flex min-h-screen flex-col-reverse items-center justify-between text-white lg:flex-row">
-          <div className="left-content text-center lg:text-left max-w-3xl space-y-4">
+          <div className="left-content z-10 text-center lg:text-left max-w-3xl space-y-4">
             <h2 className="text-6xl md:text-7xl lg:text-8xl 2xl:text-9xl text-center mt-5 lg:text-left uppercase font-semibold">Style <br /> Meets Precision</h2>
             <p className="text-light-gray max-w-xl">Whether you're here for a fresh fade or a full makeover, we’ve got you expertly covered by our talented team of expert stylists.</p>
             <div className="cta-btn uppercase mx-auto lg:mx-0 bg-light-dark w-fit px-5 py-3 md:py-4 rounded text-sm border border-transparent hover:border-light-orange cursor-pointer">Book Appointment</div>
