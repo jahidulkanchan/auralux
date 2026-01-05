bd6c44977780e460e7c80eb8349baaef3fd550fc
jahidulkanchan <mdj524156@gmail.com>
2026-01-05 11:52:13 -0800

    added hero section

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import LightEffect from "../../assets/videos/run-light-effect.mp4"
+import LightEffect from "../../assets/videos/run-light-effect.mp4";
 import HeroBanner from "../../assets/images/hero-banner.webp";
 import { Link } from "react-router";
+import BadgeImage from "../../assets/images/hero-small.webp";
 
 export default function HeroSection() {
   return (
     <>
-      <section className="relative bg-dark">
+      <section className="bg-dark relative">
         <video
           className="absolute top-0 left-0 z-[0] h-full w-full object-cover"
           muted
@@ -14,18 +15,38 @@ export default function HeroSection() {
           autoPlay
           src={LightEffect}
         ></video>
-      
-        <div className="z-10 container px-3 py-10 mx-auto gap-5 flex min-h-[650px] flex-col-reverse items-center justify-between text-white lg:flex-row">
-          <div className="left-content z-10 text-center lg:text-left max-w-3xl space-y-4">
-            <h2 className="text-6xl md:text-7xl lg:text-8xl 2xl:text-9xl text-center mt-5 lg:text-left uppercase font-semibold">Style <br /> Meets Precision</h2>
-            <p className="text-light-gray max-w-xl">Whether you're here for a fresh fade or a full makeover, we’ve got you expertly covered by our talented team of expert stylists.</p>
-            <Link to="/contact" className="cta-btn uppercase mx-auto lg:mx-0 bg-light-dark w-fit px-5 py-3 md:py-4 rounded text-sm border border-transparent hover:border-light-orange cursor-pointer">Book Appointment</Link>
+
+        <div className="z-10 container mx-auto flex min-h-[650px] flex-col-reverse items-center justify-between gap-5 px-3 py-10 text-white md:py-20 lg:flex-row">
+          <div className="left-content z-10 max-w-3xl space-y-4 text-center lg:text-left">
+            <h2 className="mt-5 text-center text-6xl font-semibold uppercase md:text-7xl lg:text-left lg:text-8xl 2xl:text-9xl">
+              Style <br />
+              <span className="flex items-center justify-center lg:justify-start gap-4">
+                <img
+                  src={BadgeImage}
+                  className="max-h-[50px] rounded-tr-2xl rounded-bl-2xl border sm:max-h-[60px] md:rounded-tr-4xl md:rounded-bl-4xl lg:max-h-[80px]"
+                  alt="hero-badge-image"
+                />
+                Meets{" "}
+              </span>
+              Precision
+            </h2>
+
+            <p className="text-light-gray mb-8 max-w-xl">
+              Whether you're here for a fresh fade or a full makeover, we’ve got
+              you expertly covered by our talented team of expert stylists.
+            </p>
+            <Link
+              to="/contact"
+              className="cta-btn bg-light-dark hover:border-light-orange mx-auto w-fit cursor-pointer rounded border border-transparent px-5 py-3 text-sm uppercase md:py-4 lg:mx-0"
+            >
+              Book Appointment
+            </Link>
           </div>
           <div className="right-content relative">
-              <div className="gradient-bg from-dark/95 to-dark absolute -bottom-10 left-0 h-[80px] w-full bg-linear-to-b blur"></div>
+            <div className="gradient-bg from-dark/95 to-dark absolute -bottom-10 left-0 h-[80px] w-full bg-linear-to-b blur"></div>
             <img
               src={HeroBanner}
-              className="w-full sm:max-w-2xl lg:max-w-3xl object-cover"
+              className="w-full object-cover sm:max-w-2xl lg:max-w-3xl"
               alt="hero-banner"
             />
           </div>
